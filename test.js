ea01fbcb64d48a777936e3bb9d0be57400cf156d
Noah-Santos <nsanto591@west-mec.org>
2023-01-13 14:23:32 -0700

    Changes
    Started the age function

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
-// // function to get passengers current age
-// function getAge(){
-//     let currentDate = new Date();
-//     console.log(currentDate);
-// }
-// console.log(getAge());
-console.log(2+2);
+// function to get passengers current age
+function getAge(){
+    // gets time and date from computer
+    let currentDate = new Date();
+    // console.log(currentDate);
+    let time = currentDate.split("T");;
+          
+}
+getAge();
