c1f179390b398486a603bce714026e0c9141c582
NoahSantos <nsanto591@west-mec.org>
2023-01-13 16:05:42 -0700

    More age
    Did more on the age function

--- test.js
+++ test.js
@@ -3,7 +3,20 @@ function getAge(){
     // gets time and date from computer
     let currentDate = new Date();
     // console.log(currentDate);
-    let time = currentDate.split("T");;
-          
+    // splits the current date into 2 pieces
+    let time = currentDate.split("T");
+    // gets rid of the second half of the array
+    time.pop();
+    // splits what is left of current date into its years, months, and days
+    time.split("-");
+    // changes all values to numbers
+    for(let d = 0; d < time.length; d++){
+        time[d] = Number(time[d]);
+    }
+    // gets date of birth and splits it and turns values into numbers
+    let birthDay = document.getElementById("birth").ariaValueMax.split("-");
+    for(let f = 0; f < birthDay.length; f++){
+        birthDay[f] = Number(birthDay[f]);
+    }
 }
 getAge();
